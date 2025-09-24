7d53e1396a7c0b49ab6042c82ee5952762e4240f
Ziqi Wei <zwei1989@uic.edu>
2025-09-24 12:46:07 -0500

    filter country and athletes

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,1 +1,15 @@
-main
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+# Read the dataset
+olympics_data <- read_csv("Olympics.csv")
+
+# 7a
+delegation_1992 <- olympics_data %>%
+  filter(year == 1992) %>%
+  arrange(desc(athletes)) %>%
+  select(country, athletes)
+
+# View the resulting tibble
+print(delegation_1992)
